97f10819537b9831af0276b7073ed86c868ee99f
BrentonPhoenix <brentonphoenix@gmail.com>
2021-10-05 09:07:37 -0400

    heroku?

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
 })
 
 module.exports = sequelize
